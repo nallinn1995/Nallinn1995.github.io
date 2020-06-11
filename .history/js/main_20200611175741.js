9b4ce09122182c0ae9725d349e96a9ec234a0570
nallinarayanan <nallinarayanan@gmail.com>
2020-06-11 18:23:22 +0530

    first commit

---
+++ .history/js/main_20200611175741.js
@@ -0,0 +1,83 @@
+"use strict";
+jQuery(document).ready(function ($) {
+
+//==========================================
+// MOBAILE MENU
+//=========================================
+
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 0)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+//==========================================
+//ScrollUp
+//=========================================
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('#scrollUp').fadeIn('slow');
+        } else {
+            $('#scrollUp]').fadeOut('slow');
+        }
+    });
+
+    $('#scrollUp').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+//==========================================
+// For fancybox active
+//=========================================
+
+    $('.fancybox').fancybox();
+    
+    
+
+//==========================================
+// Loading
+//=========================================
+
+    $(window).load(function () {
+        $("#loading").fadeOut(500);
+    });
+
+
+
+        window.onscroll = function() {myFunction()};
+        
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        
+        function myFunction() {
+          if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+        }
+       
+
+        $('.navbar div a').click(function(event){
+            //remove all pre-existing active classes
+            $('.active').removeClass('active');
+    
+            //add the active class to the link we clicked
+            $(this).addClass('active');
+        });
+});
---
+++ .history/js/main_20200611175934.js
@@ -0,0 +1,76 @@
+"use strict";
+jQuery(document).ready(function ($) {
+
+//==========================================
+// MOBAILE MENU
+//=========================================
+
+    $('#navbar-menu').find('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: (target.offset().top - 0)
+                }, 1000);
+                if ($('.navbar-toggle').css('display') != 'none') {
+                    $(this).parents('.container').find(".navbar-toggle").trigger("click");
+                }
+                return false;
+            }
+        }
+    });
+
+
+//==========================================
+//ScrollUp
+//=========================================
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('#scrollUp').fadeIn('slow');
+        } else {
+            $('#scrollUp]').fadeOut('slow');
+        }
+    });
+
+    $('#scrollUp').click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+        return false;
+    });
+
+
+
+//==========================================
+// For fancybox active
+//=========================================
+
+    $('.fancybox').fancybox();
+    
+    
+
+//==========================================
+// Loading
+//=========================================
+
+    $(window).load(function () {
+        $("#loading").fadeOut(500);
+    });
+
+
+
+        window.onscroll = function() {myFunction()};
+        
+        var navbar = document.getElementById("navbar");
+        var sticky = navbar.offsetTop;
+        
+        function myFunction() {
+          if (window.pageYOffset >= sticky) {
+            navbar.classList.add("sticky")
+          } else {
+            navbar.classList.remove("sticky");
+          }
+        }
+       
+
+});
